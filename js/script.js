4b59f1175ccafe41b09a799341ac3e675cd9130e
nardo88 <nardo1988@mail.ru>
2020-09-21 11:02:11 +0300

    create: добавили слайдер и настройли переключение слайдов кнопками с клавиатуры

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    navigation: {
+        nextEl: '.slider-hotel__btn-next',
+        prevEl: '.slider-hotel__btn-prev',
+        
+      },
+    keyboard: {
+        enabled: true,
+    }
+})
+
+
